1fa65121e57d058d79e3059b7a60f54eb9dce84b
Nashendra Fowsar <ardnehsan@gmail.com>
2017-12-18 18:01:45 -0600

    complete first part

---
+++ bamazonCustomer.js
@@ -0,0 +1,73 @@
+var mysql = require('mysql');
+var inquirer = require('inquirer');
+var table = require('cli-table');
+var connection = mysql.createConnection({
+
+    host: "localhost",
+    port: 3306,
+    user:"root",
+    password:"nf4094",
+    database:"bamazon"
+})
+
+connection.connect(function(err){
+    console.log("Connected as id: " + connection.threadId);
+
+})
+
+function showTable(){
+connection.query('SELECT * FROM products', function(err, res){
+
+    var display = new table({
+        head: ['Id:', 'Product','Price','Quantity'],
+        colWidths: [5,30,10,10]
+    });
+    for(i=0;i<res.length;i++)
+     {
+        display.push([res[i].item_id, res[i].product_name,"$" + res[i].price,res[i].stock_quantity]);
+     }
+     console.log(display.toString());
+    });
+
+}
+
+function start(){
+    showTable();
+    
+    inquirer.prompt([
+    {
+        name:"item",
+        type:"input",
+        message: "What is the ID of the item you would like to buy?",
+    },
+    {
+        name:"quantity",
+        type:"input",
+        message: "How many?"
+    
+    },
+    ]).then(function(answer){
+        var theID = answer.item;
+        var amount = answer.quantity;
+        purchase(theID,amount);
+    });
+}
+
+start();
+
+function purchase(id, quantity){
+    connection.query('SELECT * FROM products WHERE item_id=?',[id], function(err,res){
+        if(quantity <= res[0].stock_quantity)
+        {
+            var total = res[0].price * quantity;
+            console.log(total);
+            var newStock = res[0].stock_quantity - quantity;
+            connection.query('UPDATE products SET stock_quantity = ? WHERE item_id = ?', [newStock, id]);
+            console.log("Purchase of " + res[0].product_name + "(x"+ quantity + ") complete!");      
+        }
+        else
+        {
+            console.log("No availability");
+        }
+    })
+}
